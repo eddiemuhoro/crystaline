02eb925e14f1675100cfbb8203a834217260930f
eddiemuhoro <eddiemuhoro@gmail.com>
2026-03-27 09:28:55 +0300

    refactor: Simplify client configuration by storing projectId and dataset in constants

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,8 +1,11 @@
 import { createClient } from "next-sanity";
 
+const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "3hro2ff6";
+const sanityDataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: sanityProjectId,
+  dataset: sanityDataset,
   apiVersion: "2024-01-01",
   useCdn: false,
 });
